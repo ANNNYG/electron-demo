b8eca36b2f4ef384008e43a36be49d3239ce6e10
Zyg <646124200@qq.com>
2023-07-09 11:40:20 +0800

    feat: 基本生命周期

--- main.js
+++ main.js
@@ -1,19 +1,44 @@
 const { app, BrowserWindow } = require("electron");
 
-app.whenReady().then(() => {
-  const mainWin = new BrowserWindow({
-    width: 600,
-    height: 400,
-  });
+function createWindow() {
+  console.log("ready");
+  app.whenReady().then(() => {
+    const mainWin = new BrowserWindow({
+      width: 800,
+      height: 400,
+    });
 
-  mainWin.loadFile("index.html");
+    mainWin.loadFile("index.html");
 
-  mainWin.on("close", () => {
-    console.log("close");
+    mainWin.webContents.on("did-finish-load", () => {
+      console.log("did-finish-load");
+    });
+
+    mainWin.webContents.on("dom-ready", () => {
+      console.log("dom-ready");
+    });
+
+    mainWin.on("close", () => {
+      console.log("close");
+    });
   });
-});
+}
+
+app.on("ready", createWindow);
 
 app.on("window-all-closed", () => {
   console.log("all windows is closed");
   app.quit();
+});
+
+app.on("before-quit", () => {
+  console.log("before-quit");
+});
+
+app.on("will-quit", () => {
+  console.log("will-quit");
+});
+
+app.on("quit", () => {
+  console.log("quit");
 });
--- readme.md
+++ readme.md
@@ -14,3 +14,14 @@
       npm config set electron_mirror "https://npm.taobao.org/mirrors/electron/"
       npm install electron --save-dev
 ```
+
+## 生命周期
+
+- ready:app 初始化完成
+- dom-ready: 一个窗口中的文本加载完成
+- did-finish-load: 导航完成时触发
+- window-all-closed: 所有窗口都被关闭时触发
+- before-quite: 在关闭窗口之前触发
+- will-quit: 在窗口关闭并且应用退出时触发
+- quite: 当所有窗口被关闭时触发
+- closed: 当窗口关闭时触发，此时应删除窗口引用
